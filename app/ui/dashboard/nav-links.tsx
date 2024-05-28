46d7c7e4ce525ace96708649acec72e4c891cc6d
Janvi <Janvipatel9720@gmail.com>
2024-05-27 20:30:27 -0400

    Updated

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -32,7 +32,7 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <link
+          <Link
             key={link.name}
             href={link.href}
             className={clsx(
@@ -44,7 +44,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </link>
+          </Link>
         );
       })}
     </>
